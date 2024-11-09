6ae9e15743ef3de253a9b64d4f3628a42e32fe4f
edicel0219 <edicelynnadlandas@gmail.com>
2024-11-09 21:32:44 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -2,35 +2,34 @@ const express = require('express');
 const app = express();
 
 
-const isPrime = (number) => {
-    if (number <= 1) {
-        return false; 
+const factorial = (number) => {
+    if (number < 0) {
+        return 'Error: Factorial is not defined for negative numbers';
     }
-
-    for (let i = 2; i <= Math.sqrt(number); i++) {
-        if (number % i === 0) {
-            return false; 
-        }
+    
+    let result = 1;
+    for (let i = 1; i <= number; i++) {
+        result *= i;
     }
-
-    return true; 
+    return result;
 };
 
-// Endpoint to check if a number is prime
-app.get('/assignments/prime/:number', (req, res) => {
+
+app.get('/assignments/factorial/:number', (req, res) => {
     const number = parseInt(req.params.number);  
 
-    // Validate the input
+
     if (isNaN(number)) {
         return res.status(400).json({ error: 'Please provide a valid number.' });
     }
 
 
-    const result = isPrime(number);
-    res.json({ number: number, isPrime: result });
+    const result = factorial(number);
+    res.json({ number: number, factorial: result });
 });
 
-const PORT = process.env.PORT || 8081;
+
+const PORT = process.env.PORT || 8082;
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
